f6f62028a9054442d143d2fe452218f76fc16f5f
Gregory Conrad <gregorysconrad@gmail.com>
2023-11-29 21:23:10 -0500

    feat: add optional eq rebuild optimization

--- rearch/src/txn.rs
+++ rearch/src/txn.rs
@@ -1,10 +1,11 @@
 use concread::hashmap::{HashMapReadTxn, HashMapWriteTxn};
 use std::{
     any::{Any, TypeId},
+    cell::OnceCell,
     collections::HashSet,
 };
 
-use crate::{Capsule, CapsuleData, CapsuleManager, CapsuleRebuilder};
+use crate::{Capsule, CapsuleData, CapsuleManager, CapsuleRebuilder, EXCLUSIVE_OWNER_MSG};
 
 #[allow(clippy::module_name_repetitions)]
 pub struct ContainerReadTxn<'a> {
@@ -75,6 +76,99 @@ impl ContainerWriteTxn<'_> {
         self.try_read_raw::<C>()
     }
 
+    /// Forcefully disposes only the requested node, cleaning up the node's direct dependencies.
+    /// Panics if the node or one of its dependencies is not in the graph.
+    pub(crate) fn dispose_node(&mut self, id: TypeId) {
+        self.data.remove(&id);
+        self.nodes
+            .remove(&id)
+            .expect("Node should be in graph")
+            .dependencies
+            .iter()
+            .for_each(|dep| {
+                self.node_or_panic(*dep).dependents.remove(&id);
+            });
+    }
+
+    pub(crate) fn add_dependency_relationship(&mut self, dependency: TypeId, dependent: TypeId) {
+        self.node_or_panic(dependency).dependents.insert(dependent);
+        self.node_or_panic(dependent)
+            .dependencies
+            .insert(dependency);
+    }
+
+    /// Forcefully builds the capsule with the supplied id. Panics if node is not in the graph
+    pub(crate) fn build_capsule_or_panic(&mut self, id: TypeId) {
+        let self_changed = self.build_single_node(id);
+        if !self_changed {
+            return;
+        }
+
+        let mut build_order_stack = self.create_build_order_stack(id);
+        build_order_stack.pop(); // we built id already above, and id is the head of the stack
+        let disposable_nodes = self.get_disposable_nodes_from_build_order_stack(&build_order_stack);
+
+        let mut changed_nodes = HashSet::new();
+        changed_nodes.insert(id);
+
+        for curr_id in build_order_stack.into_iter().rev() {
+            let node = self.node_or_panic(curr_id);
+
+            let have_deps_changed = node
+                .dependencies
+                .iter()
+                .any(|dep| changed_nodes.contains(dep));
+            if !have_deps_changed {
+                continue;
+            }
+
+            if disposable_nodes.contains(&curr_id) {
+                // Note: dependency/dependent relationships will be ok after this,
+                // since we are disposing all dependents in the build order,
+                // because we are adding this node to changedNodes
+                self.dispose_single_node(curr_id);
+                changed_nodes.insert(curr_id);
+            } else {
+                let did_node_change = self.build_single_node(curr_id);
+                if did_node_change {
+                    changed_nodes.insert(curr_id);
+                }
+            }
+        }
+    }
+
+    pub(crate) fn take_capsule_and_side_effect(
+        &mut self,
+        id: TypeId,
+    ) -> (Box<dyn Any + Send>, OnceCell<Box<dyn Any + Send>>) {
+        let node = self.node_or_panic(id);
+        let capsule = std::mem::take(&mut node.capsule).expect(EXCLUSIVE_OWNER_MSG);
+        let side_effect = std::mem::take(&mut node.side_effect).expect(EXCLUSIVE_OWNER_MSG);
+        (capsule, side_effect)
+    }
+
+    pub(crate) fn yield_capsule_and_side_effect(
+        &mut self,
+        id: TypeId,
+        capsule: Box<dyn Any + Send>,
+        side_effect: OnceCell<Box<dyn Any + Send>>,
+    ) {
+        let node = self.node_or_panic(id);
+        assert!(
+            node.capsule.is_none() && node.side_effect.is_none(),
+            "Manager had ownership over a capsule and side effect when ownership was yielded back",
+        );
+        node.capsule = Some(capsule);
+        node.side_effect = Some(side_effect);
+    }
+
+    pub(crate) fn side_effect(&mut self, id: TypeId) -> &mut OnceCell<Box<dyn Any + Send>> {
+        self.node_or_panic(id)
+            .side_effect
+            .as_mut()
+            .expect(EXCLUSIVE_OWNER_MSG)
+    }
+
     /// Tries a capsule read, but doesn't require an instance of the capsule itself
     fn try_read_raw<C: Capsule>(&self) -> Option<C::Data> {
         let id = TypeId::of::<C>();
@@ -97,30 +191,19 @@ impl ContainerWriteTxn<'_> {
         self.build_capsule_or_panic(id);
     }
 
-    /// Forcefully builds the capsule with the supplied id. Panics if node is not in the graph
-    pub(crate) fn build_capsule_or_panic(&mut self, id: TypeId) {
-        self.build_single_node(id);
-
-        // Since we have already built the node above (since *it must be built in this method*),
-        // we can skip it with skip(1) when we are handling the rest of the dependent subgraph
-        let build_order = {
-            let build_order = self.create_build_order_stack(id).into_iter().rev().skip(1);
-            self.garbage_collect_diposable_nodes(build_order)
-        };
-        for id in build_order {
-            self.build_single_node(id);
-        }
+    /// Gets the requested node if it is in the graph
+    fn node(&mut self, id: TypeId) -> Option<&mut CapsuleManager> {
+        self.nodes.get_mut(&id)
     }
 
     /// Gets the requested node or panics if it is not in the graph
-    pub(crate) fn node_or_panic(&mut self, id: TypeId) -> &mut CapsuleManager {
-        self.nodes
-            .get_mut(&id)
+    fn node_or_panic(&mut self, id: TypeId) -> &mut CapsuleManager {
+        self.node(id)
             .expect("Requested node should be in the graph")
     }
 
     /// Builds only the requested node. Panics if the node is not in the graph
-    fn build_single_node(&mut self, id: TypeId) {
+    fn build_single_node(&mut self, id: TypeId) -> bool {
         // Remove old dependency info since it may change on this build
         // We use std::mem::take below to prevent needing a clone on the existing dependencies
         let node = self.node_or_panic(id);
@@ -130,12 +213,15 @@ impl ContainerWriteTxn<'_> {
         }
 
         // Trigger the build (which also populates its new dependencies in self)
-        (self.node_or_panic(id).build)(self);
+        (self.node_or_panic(id).build)(self)
     }
 
-    /// Forcefully disposes only the requested node, cleaning up the node's direct dependencies.
-    /// Panics if the node is not in the graph.
-    pub(crate) fn dispose_single_node(&mut self, id: TypeId) {
+    /// Disposes just the supplied node, and *attempts* to clean up the node's direct dependencies.
+    /// *This is meant to be a helper only for [`build_capsule_or_panic`]*,
+    /// as an idempotent node getting disposed in that method may have dependencies that
+    /// were already disposed from the graph.
+    /// In all other cases, [`dispose_node`] is likely the proper method to use.
+    fn dispose_single_node(&mut self, id: TypeId) {
         self.data.remove(&id);
         self.nodes
             .remove(&id)
@@ -143,7 +229,9 @@ impl ContainerWriteTxn<'_> {
             .dependencies
             .iter()
             .for_each(|dep| {
-                self.node_or_panic(*dep).dependents.remove(&id);
+                if let Some(node) = self.node(*dep) {
+                    node.dependents.remove(&id);
+                }
             });
     }
 
@@ -177,28 +265,27 @@ impl ContainerWriteTxn<'_> {
         build_order_stack
     }
 
-    /// Helper function that given a `build_order`, garbage collects all idempotent nodes
-    /// that have no dependents (i.e., they are entirely disposable)
-    /// and returns the new build order without the (now garbage collected) idempotent nodes.
+    /// Helper function that finds all idempotent capsules with no nonidempotent downstream
+    /// capsules, given a `build_order_stack` (a *reversed build order*).
+    ///
     /// While the build order specifies the order in which nodes must be built in to propagate
     /// updates, the reverse of the build order specifies the order in which we can trim down
     /// some fat through gc.
-    // TODO(GregoryConrad): update this to use the mechanism currently used by rearch-dart
-    fn garbage_collect_diposable_nodes(
+    fn get_disposable_nodes_from_build_order_stack(
         &mut self,
-        build_order: impl DoubleEndedIterator<Item = TypeId>,
-    ) -> impl DoubleEndedIterator<Item = TypeId> {
-        let mut non_disposable = Vec::new();
+        build_order_stack: &Vec<TypeId>,
+    ) -> HashSet<TypeId> {
+        let mut disposable = HashSet::new();
 
-        build_order.rev().for_each(|id| {
-            let is_disposable = self.node_or_panic(id).is_disposable();
-            if is_disposable {
-                self.dispose_single_node(id);
-            } else {
-                non_disposable.push(id);
+        for id in build_order_stack {
+            let node = self.node_or_panic(*id);
+            let dependents_all_disposable =
+                node.dependents.iter().all(|dep| disposable.contains(dep));
+            if node.is_idempotent() && dependents_all_disposable {
+                disposable.insert(*id);
             }
-        });
+        }
 
-        non_disposable.into_iter().rev()
+        disposable
     }
 }
