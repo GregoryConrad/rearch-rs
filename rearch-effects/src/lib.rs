ffda1036991653439cb71eb34bdae3cba710b065
Gregory Conrad <gregorysconrad@gmail.com>
2024-01-01 18:45:04 +1000

    refactor!: switch SideEffect to GAT lifetime (#22)
    Fixes #3 (thanks for the initial idea of the workaround @0e4ef622!)

--- rearch-effects/src/lib.rs
+++ rearch-effects/src/lib.rs
@@ -1,43 +1,114 @@
 use rearch::{CData, SideEffect, SideEffectRegistrar};
 use std::{cell::OnceCell, sync::Arc};
 
-pub fn raw<'a, T: Send + 'static>(
+// This workaround was derived from:
+// https://github.com/GregoryConrad/rearch-rs/issues/3#issuecomment-1872869363
+// And is needed because of:
+// https://github.com/rust-lang/rust/issues/111662
+use effect_lifetime_fixers::{EffectLifetimeFixer0, EffectLifetimeFixer1, EffectLifetimeFixer2};
+mod effect_lifetime_fixers {
+    use rearch::{SideEffect, SideEffectRegistrar};
+
+    pub struct EffectLifetimeFixer0<F>(F);
+    impl<T, F> SideEffect for EffectLifetimeFixer0<F>
+    where
+        T: Send + 'static,
+        F: FnOnce(SideEffectRegistrar) -> &mut T,
+    {
+        type Api<'a> = &'a mut T;
+        fn build(self, registrar: SideEffectRegistrar) -> Self::Api<'_> {
+            self.0(registrar)
+        }
+    }
+    impl<F> EffectLifetimeFixer0<F> {
+        pub(super) const fn new<T>(f: F) -> Self
+        where
+            F: FnOnce(SideEffectRegistrar) -> &mut T,
+        {
+            Self(f)
+        }
+    }
+
+    pub struct EffectLifetimeFixer1<F>(F);
+    impl<T, F, R1> SideEffect for EffectLifetimeFixer1<F>
+    where
+        T: Send + 'static,
+        F: FnOnce(SideEffectRegistrar) -> (&mut T, R1),
+    {
+        type Api<'a> = (&'a mut T, R1);
+        fn build(self, registrar: SideEffectRegistrar) -> Self::Api<'_> {
+            self.0(registrar)
+        }
+    }
+    impl<F> EffectLifetimeFixer1<F> {
+        pub(super) const fn new<T, R1>(f: F) -> Self
+        where
+            F: FnOnce(SideEffectRegistrar) -> (&mut T, R1),
+        {
+            Self(f)
+        }
+    }
+
+    pub struct EffectLifetimeFixer2<F>(F);
+    impl<T, F, R1, R2> SideEffect for EffectLifetimeFixer2<F>
+    where
+        T: Send + 'static,
+        F: FnOnce(SideEffectRegistrar) -> (&mut T, R1, R2),
+    {
+        type Api<'a> = (&'a mut T, R1, R2);
+        fn build(self, registrar: SideEffectRegistrar) -> Self::Api<'_> {
+            self.0(registrar)
+        }
+    }
+    impl<F> EffectLifetimeFixer2<F> {
+        pub(super) const fn new<T, R1, R2>(f: F) -> Self
+        where
+            F: FnOnce(SideEffectRegistrar) -> (&mut T, R1, R2),
+        {
+            Self(f)
+        }
+    }
+}
+
+pub fn raw<T: Send + 'static>(
     initial: T,
-) -> impl SideEffect<
-    'a,
-    Api = (
+) -> impl for<'a> SideEffect<
+    Api<'a> = (
         &'a mut T,
         impl CData + Fn(Box<dyn FnOnce(&mut T)>),
         Arc<dyn Send + Sync + Fn(Box<dyn FnOnce()>)>,
     ),
 > {
-    move |register: SideEffectRegistrar<'a>| register.raw(initial)
+    EffectLifetimeFixer2::new(move |register: SideEffectRegistrar| register.raw(initial))
 }
 
+// NOTE: returns (), the no-op side effect
 #[must_use]
-pub fn as_listener<'a>() -> impl SideEffect<'a, Api = ()> {} // returns (), the no-op side effect
+pub fn as_listener() -> impl for<'a> SideEffect<Api<'a> = ()> {}
 
-pub fn state<'a, T: Send + 'static>(
+pub fn state<T: Send + 'static>(
     initial: T,
-) -> impl SideEffect<'a, Api = (&'a mut T, impl CData + Fn(T))> {
-    move |register: SideEffectRegistrar<'a>| {
+) -> impl for<'a> SideEffect<Api<'a> = (&'a mut T, impl CData + Fn(T))> {
+    EffectLifetimeFixer1::new(move |register: SideEffectRegistrar| {
         let (state, rebuild, _) = register.raw(initial);
         let set_state = move |new_state| {
             rebuild(Box::new(|state| *state = new_state));
         };
         (state, set_state)
-    }
+    })
 }
 
 // This uses a hacked together Lazy implementation because LazyCell doesn't have force_mut;
 // see https://github.com/rust-lang/rust/issues/109736#issuecomment-1605787094
 #[allow(clippy::missing_panics_doc)] // false positive
-pub fn lazy_state<'a, T, F>(init: F) -> impl SideEffect<'a, Api = (&'a mut T, impl CData + Fn(T))>
+pub fn lazy_state<T, F>(
+    init: F,
+) -> impl for<'a> SideEffect<Api<'a> = (&'a mut T, impl CData + Fn(T))>
 where
     T: Send + 'static,
     F: FnOnce() -> T + Send + 'static,
 {
-    move |register: SideEffectRegistrar<'a>| {
+    EffectLifetimeFixer1::new(move |register: SideEffectRegistrar| {
         let ((cell, f), rebuild, _) = register.raw((OnceCell::new(), Some(init)));
         cell.get_or_init(|| std::mem::take(f).expect("Init fn should be present for cell init")());
         let state = cell.get_mut().expect("State initialized above");
@@ -48,34 +119,31 @@ where
             }));
         };
         (state, set_state)
-    }
+    })
 }
 
-pub fn value<'a, T: Send + 'static>(value: T) -> impl SideEffect<'a, Api = &'a mut T> {
-    move |register: SideEffectRegistrar<'a>| {
-        let (state, _, _) = register.raw(value);
-        state
-    }
+pub fn value<T: Send + 'static>(value: T) -> impl for<'a> SideEffect<Api<'a> = &'a mut T> {
+    EffectLifetimeFixer0::new(move |register: SideEffectRegistrar| register.raw(value).0)
 }
 
 // This uses a hacked together Lazy implementation because LazyCell doesn't have force_mut;
 // see https://github.com/rust-lang/rust/issues/109736#issuecomment-1605787094
 #[allow(clippy::missing_panics_doc)] // false positive
-pub fn lazy_value<'a, T, F>(init: F) -> impl SideEffect<'a, Api = &'a mut T>
+pub fn lazy_value<T, F>(init: F) -> impl for<'a> SideEffect<Api<'a> = &'a mut T>
 where
     T: Send + 'static,
     F: FnOnce() -> T + Send + 'static,
 {
-    move |register: SideEffectRegistrar<'a>| {
+    EffectLifetimeFixer0::new(move |register: SideEffectRegistrar| {
         let ((cell, f), _, _) = register.raw((OnceCell::new(), Some(init)));
         cell.get_or_init(|| std::mem::take(f).expect("Init fn should be present for cell init")());
         cell.get_mut().expect("State initialized above")
-    }
+    })
 }
 
 #[must_use]
-pub fn is_first_build<'a>() -> impl SideEffect<'a, Api = bool> {
-    move |register: SideEffectRegistrar<'a>| {
+pub fn is_first_build() -> impl for<'a> SideEffect<Api<'a> = bool> {
+    move |register: SideEffectRegistrar| {
         let has_built_before = register.register(value(false));
         let is_first_build = !*has_built_before;
         *has_built_before = true;
@@ -88,30 +156,22 @@ pub fn is_first_build<'a>() -> impl SideEffect<'a, Api = bool> {
 /// Only use this side effect when you don't have any state that you wish to update in the rebuild
 /// (which is extremely rare).
 #[must_use]
-pub fn rebuilder<'a>() -> impl SideEffect<'a, Api = impl CData + Fn()> {
-    move |register: SideEffectRegistrar<'a>| {
+pub fn rebuilder() -> impl for<'a> SideEffect<Api<'a> = impl CData + Fn()> {
+    move |register: SideEffectRegistrar| {
         let ((), rebuild, _) = register.raw(());
         move || rebuild(Box::new(|()| {}))
     }
 }
 
-pub fn run_once<'a, F>(f: F) -> impl SideEffect<'a, Api = ()>
-where
-    F: FnOnce() + Send + 'static,
-{
-    move |register: SideEffectRegistrar<'a>| {
-        register.register(lazy_value(f));
-    }
-}
-
+/*
 /// Side effect that runs a callback whenever it changes and is dropped.
 /// Similar to `useEffect` from React.
 #[must_use]
-pub fn run_on_change<'a, F>() -> impl SideEffect<'a, Api = impl FnMut(F) + 'a>
+pub fn run_on_change<F>() -> impl for<'a> SideEffect<Api<'a> = impl FnMut(F) + 'a>
 where
     F: FnOnce() + Send + 'static,
 {
-    move |register: SideEffectRegistrar<'a>| {
+    move |register: SideEffectRegistrar| {
         let state = register.register(value(FunctionalDrop(None)));
         // The old callback, if there is one, will be called when it is dropped,
         // via the `*state = ...` assignment below
@@ -126,16 +186,34 @@ impl<F: FnOnce()> Drop for FunctionalDrop<F> {
         }
     }
 }
+#[must_use]
+pub fn run_on_change2<F>() -> RunOnChange<F>
+where
+    F: FnOnce() + Send + 'static,
+{
+    RunOnChange(std::marker::PhantomData)
+}
+pub struct RunOnChange<F>(std::marker::PhantomData<F>);
+impl<F: Send + FnOnce() + 'static> SideEffect for RunOnChange<F> {
+    type Api<'registrar> = impl FnMut(F) + 'registrar;
 
-pub fn reducer<'a, State, Action, Reducer>(
+    fn build(self, registrar: SideEffectRegistrar) -> Self::Api<'_> {
+        let state = registrar.register(value(FunctionalDrop(None)));
+        // The old callback, if there is one, will be called when it is dropped,
+        // via the `*state = ...` assignment below
+        |callback| *state = FunctionalDrop(Some(callback))
+    }
+}
+
+pub fn reducer<State, Action, Reducer>(
     reducer: Reducer,
     initial: State,
-) -> impl SideEffect<'a, Api = (State, impl CData + Fn(Action))>
+) -> impl for<'a> SideEffect<Api<'a> = (State, impl CData + Fn(Action))>
 where
     State: Clone + Send + Sync + 'static,
     Reducer: Fn(State, Action) -> State + Clone + Send + Sync + 'static,
 {
-    move |register: SideEffectRegistrar<'a>| {
+    move |register: SideEffectRegistrar<'_>| {
         let (state, set_state) = register.register(state(initial));
         (state.clone(), {
             let state = state.clone();
@@ -147,15 +225,15 @@ where
     }
 }
 
-pub fn lazy_reducer<'a, State, Action, Reducer>(
+pub fn lazy_reducer<State, Action, Reducer>(
     reducer: Reducer,
     initial: impl FnOnce() -> State + Send + 'static,
-) -> impl SideEffect<'a, Api = (State, impl CData + Fn(Action))>
+) -> impl for<'a> SideEffect<Api<'a> = (State, impl CData + Fn(Action))>
 where
     State: Clone + Send + Sync + 'static,
     Reducer: Fn(State, Action) -> State + Clone + Send + Sync + 'static,
 {
-    move |register: SideEffectRegistrar<'a>| {
+    move |register: SideEffectRegistrar| {
         let (state, set_state) = register.register(lazy_state(initial));
         (state.clone(), {
             let state = state.clone();
@@ -167,7 +245,7 @@ where
     }
 }
 
-/*
+// TODO should this actually be hydrate like in Dart?
 /// A thin wrapper around the state side effect that enables easy state persistence.
 ///
 /// You provide a `read` function and a `write` function,
@@ -177,17 +255,17 @@ where
 /// Note: when possible, it is highly recommended to use async persist instead of sync persist.
 /// This effect is blocking, which will prevent other capsule updates.
 /// However, this function is perfect for quick I/O, like when using something similar to redb.
-pub fn sync_persist<'a, Read, Write, R, T>(
+pub fn sync_persist<Read, Write, R, T>(
     read: Read,
     write: Write,
-) -> impl SideEffect<'a, Api = (&'a R, impl CData + Fn(T))>
+) -> impl for<'a> SideEffect<Api<'a> = (&'a R, impl CData + Fn(T))>
 where
     T: Send + 'static,
     R: Send + 'static,
     Read: FnOnce() -> R + Send + 'static,
     Write: Fn(T) -> R + Send + Sync + 'static,
 {
-    move |register: SideEffectRegistrar<'a>| {
+    move |register: SideEffectRegistrar| {
         let (state, set_state) = register.register(lazy_state(read));
         let write = Arc::new(write);
         let persist = move |new_data| {
