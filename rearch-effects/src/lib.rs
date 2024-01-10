b5449583ba399834b01733143fbb80356b4453bf
Gregory Conrad <gregorysconrad@gmail.com>
2024-01-10 08:26:44 -1000

    feat!: re-add and modernize older side effects

--- rearch-effects/src/lib.rs
+++ rearch-effects/src/lib.rs
@@ -1,5 +1,6 @@
+use once_cell::unsync::Lazy;
 use rearch::{CData, SideEffect, SideEffectRegistrar};
-use std::{cell::OnceCell, sync::Arc};
+use std::sync::Arc;
 
 pub mod cloneable;
 
@@ -100,8 +101,6 @@ pub fn state<T: Send + 'static>(
     })
 }
 
-// This uses a hacked together Lazy implementation because LazyCell doesn't have force_mut;
-// see https://github.com/rust-lang/rust/issues/109736#issuecomment-1605787094
 #[allow(clippy::missing_panics_doc)] // false positive
 pub fn lazy_state<T, F>(
     init: F,
@@ -111,16 +110,11 @@ where
     F: FnOnce() -> T + Send + 'static,
 {
     EffectLifetimeFixer1::new(move |register: SideEffectRegistrar| {
-        let ((cell, f), rebuild, _) = register.raw((OnceCell::new(), Some(init)));
-        cell.get_or_init(|| std::mem::take(f).expect("Init fn should be present for cell init")());
-        let state = cell.get_mut().expect("State initialized above");
+        let (cell, rebuild, _) = register.raw(Lazy::new(init));
         let set_state = move |new_state| {
-            rebuild(Box::new(|effect| {
-                effect.0.take();
-                _ = effect.0.set(new_state);
-            }));
+            rebuild(Box::new(|cell| **cell = new_state));
         };
-        (state, set_state)
+        (&mut **cell, set_state)
     })
 }
 
@@ -128,8 +122,6 @@ pub fn value<T: Send + 'static>(value: T) -> impl for<'a> SideEffect<Api<'a> = &
     EffectLifetimeFixer0::new(move |register: SideEffectRegistrar| register.raw(value).0)
 }
 
-// This uses a hacked together Lazy implementation because LazyCell doesn't have force_mut;
-// see https://github.com/rust-lang/rust/issues/109736#issuecomment-1605787094
 #[allow(clippy::missing_panics_doc)] // false positive
 pub fn lazy_value<T, F>(init: F) -> impl for<'a> SideEffect<Api<'a> = &'a mut T>
 where
@@ -137,9 +129,8 @@ where
     F: FnOnce() -> T + Send + 'static,
 {
     EffectLifetimeFixer0::new(move |register: SideEffectRegistrar| {
-        let ((cell, f), _, _) = register.raw((OnceCell::new(), Some(init)));
-        cell.get_or_init(|| std::mem::take(f).expect("Init fn should be present for cell init")());
-        cell.get_mut().expect("State initialized above")
+        let (cell, _, _) = register.raw(Lazy::new(init));
+        &mut **cell
     })
 }
 
@@ -153,18 +144,57 @@ pub fn is_first_build() -> impl for<'a> SideEffect<Api<'a> = bool> {
     }
 }
 
-/// A side effect that provides a function that triggers rebuilds.
-/// YOU SHOULD ALMOST NEVER USE THIS SIDE EFFECT!
-/// Only use this side effect when you don't have any state that you wish to update in the rebuild
-/// (which is extremely rare).
-#[must_use]
-pub fn rebuilder() -> impl for<'a> SideEffect<Api<'a> = impl CData + Fn()> {
-    move |register: SideEffectRegistrar| {
-        let ((), rebuild, _) = register.raw(());
-        move || rebuild(Box::new(|()| {}))
-    }
+pub fn reducer<State, Action, Reducer>(
+    reducer: Reducer,
+    initial: State,
+) -> impl for<'a> SideEffect<Api<'a> = (&'a mut State, impl CData + Fn(Action))>
+where
+    State: Send + 'static,
+    Action: 'static,
+    Reducer: Clone + Send + Sync + 'static + Fn(&State, Action) -> State,
+{
+    EffectLifetimeFixer1::new(move |register: SideEffectRegistrar| {
+        let (state, update_state, _) = register.raw(initial);
+        (state, move |action| {
+            let reducer = reducer.clone();
+            update_state(Box::new(move |state| *state = reducer(state, action)));
+        })
+    })
 }
 
+// NOTE: Commented out because:
+// - This fails to compile due to a compiler bug (&'a mut R compiles fine, but &'a R doesn't)
+// - I think people should really be using a hydrate equivalent instead of this
+//   - A combo of lazy_value and run_on_change probably
+//
+// /// A thin wrapper around the state side effect that enables easy state persistence.
+// ///
+// /// You provide a `read` function and a `write` function,
+// /// and you receive the status of the latest read/write operation,
+// /// in addition to a persist function that persists new state and triggers rebuilds.
+// ///
+// /// Note: when possible, it is highly recommended to use async persist instead of sync persist.
+// /// This effect is blocking, which will prevent other capsule updates.
+// /// However, this function is perfect for quick I/O, like when using something similar to redb.
+// pub fn persist<Read, Write, R, T>(
+//     read: Read,
+//     write: Write,
+// ) -> impl for<'a> SideEffect<Api<'a> = (&'a R, impl CData + Fn(T))>
+// where
+//     T: Send + 'static,
+//     R: Send + 'static,
+//     Read: Send + 'static + FnOnce() -> R,
+//     Write: Clone + Send + Sync + 'static + Fn(T) -> R,
+// {
+//     EffectLifetimeFixer1::new(move |register: SideEffectRegistrar| {
+//         let (state, set_state) = register.register(lazy_state(read));
+//         let persist = move |new_data| set_state(write(new_data));
+//         (&*state, persist)
+//     })
+// }
+
+// NOTE: Commented out because this currently fails to compile due to the
+// higher kinded lifetime bound on the nested opaque type (Api<'a> = impl Trait + 'a)
 /*
 /// Side effect that runs a callback whenever it changes and is dropped.
 /// Similar to `useEffect` from React.
@@ -204,77 +234,6 @@ impl<F: Send + FnOnce() + 'static> SideEffect for RunOnChange<F> {
         // The old callback, if there is one, will be called when it is dropped,
         // via the `*state = ...` assignment below
         |callback| *state = FunctionalDrop(Some(callback))
-    }
-}
-
-pub fn reducer<State, Action, Reducer>(
-    reducer: Reducer,
-    initial: State,
-) -> impl for<'a> SideEffect<Api<'a> = (State, impl CData + Fn(Action))>
-where
-    State: Clone + Send + Sync + 'static,
-    Reducer: Fn(State, Action) -> State + Clone + Send + Sync + 'static,
-{
-    move |register: SideEffectRegistrar<'_>| {
-        let (state, set_state) = register.register(state(initial));
-        (state.clone(), {
-            let state = state.clone();
-            move |action| {
-                let state = state.clone();
-                set_state(reducer(state, action));
-            }
-        })
-    }
-}
-
-pub fn lazy_reducer<State, Action, Reducer>(
-    reducer: Reducer,
-    initial: impl FnOnce() -> State + Send + 'static,
-) -> impl for<'a> SideEffect<Api<'a> = (State, impl CData + Fn(Action))>
-where
-    State: Clone + Send + Sync + 'static,
-    Reducer: Fn(State, Action) -> State + Clone + Send + Sync + 'static,
-{
-    move |register: SideEffectRegistrar| {
-        let (state, set_state) = register.register(lazy_state(initial));
-        (state.clone(), {
-            let state = state.clone();
-            move |action| {
-                let state = state.clone();
-                set_state(reducer(state, action));
-            }
-        })
-    }
-}
-
-// TODO should this actually be hydrate like in Dart?
-/// A thin wrapper around the state side effect that enables easy state persistence.
-///
-/// You provide a `read` function and a `write` function,
-/// and you receive the status of the latest read/write operation,
-/// in addition to a persist function that persists new state and triggers rebuilds.
-///
-/// Note: when possible, it is highly recommended to use async persist instead of sync persist.
-/// This effect is blocking, which will prevent other capsule updates.
-/// However, this function is perfect for quick I/O, like when using something similar to redb.
-pub fn sync_persist<Read, Write, R, T>(
-    read: Read,
-    write: Write,
-) -> impl for<'a> SideEffect<Api<'a> = (&'a R, impl CData + Fn(T))>
-where
-    T: Send + 'static,
-    R: Send + 'static,
-    Read: FnOnce() -> R + Send + 'static,
-    Write: Fn(T) -> R + Send + Sync + 'static,
-{
-    move |register: SideEffectRegistrar| {
-        let (state, set_state) = register.register(lazy_state(read));
-        let write = Arc::new(write);
-        let persist = move |new_data| {
-            let persist_result = write(new_data);
-            set_state(persist_result);
-        };
-        (&*state, persist)
     }
 }
 */
